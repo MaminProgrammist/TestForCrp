1b4b0b1c65113fbf45cdfeacf13ed057920e8a78
MaminProgrammist <kit04048@gmail.com>
2023-12-08 19:30:20 +0300

    added UI

--- Assets/Zone.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public class Zone : MonoBehaviour
-{
-    [SerializeField, Range(0, 1)] private float _timeSlowdown;
-
-    public float GetSlowdownValue() => _timeSlowdown;
-}
